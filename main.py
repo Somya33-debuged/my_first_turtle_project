235db4b487035ebd454c556659bf781f16d92567
Somya33-debuged <somyak288@gmail.com>
2025-06-15 05:01:43 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,75 @@
 import random
-def format_data(account):
-  """Format the account data into printable format"""
-  account_name = account["name"]
-  account_descr = account["description"]
-  account_country = account["country"]
-  return f"{account_name}, a {account_descr}, from {account_country}"
-def check_answer(guess, a_followers, b_followers):
-  """Take the user guess and follower counts and returns if they got it right"""
-  if a_followers > b_followers:
-    return guess == "a"
+from art import logo
+print("Welcome to the BLACKJACK!,the game of luck and skill")
+print("RULES [hit = get another card and stand = stay with the cards you have.  if you go over 21 you lose. If you get 21 you win. if the dealer goes over 21 you win. if the dealer and you both go over 21..then the person with low value wins]") 
+def deal_card():
+  """Returns a random card from the deck."""
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+def deal_card():
+  """Returns a random card from the deck."""
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+def calculate_score(cards):
+  """Take a list of cards and return the score calculated from the cards"""
+  if sum(cards) == 21 and len(cards) == 2:
+    return 0
+  if 11 in cards and sum(cards) > 21:
+    cards.remove(11)
+    cards.append(1)
+  return sum(cards)
+def compare(user_score, computer_score):
+  if user_score > 21 and computer_score > 21:
+    return "You went over. You lose"
+  if user_score == computer_score:
+    return "Draw 🙃"
+  elif computer_score == 0:
+    return "Lose, opponent has Blackjack"
+  elif user_score == 0:
+    return "Win with a Blackjack"
+  elif user_score > 21:
+    return "You went over. You lose"
+  elif computer_score > 21:
+    return "Opponent went over. You win"
+  elif user_score > computer_score:
+    return "You win"
   else:
-    return guess == "b"
-def game():
-  from art import logo, vs
-  from game_data import data
+    return "You lose"
+
+def play_game():
+
   print(logo)
-  score = 0
-  game_should_continue = True  
-  account_a = random.choice(data)
-  account_b = random.choice(data)
-  while game_should_continue:
-   account_a = account_b
-   account_b = random.choice(data)
-   while account_a == account_b:
-     account_b = random.choice(data)
-   print(f"Compare A: {format_data(account_a)}")
-   print(vs)
-   print(f"Against B: {format_data(account_b)}")
-   guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-   a_follower_count = account_a["follower_count"]
-   b_follower_count = account_b["follower_count"]
-   is_correct = check_answer(guess, a_follower_count, b_follower_count)
-   if is_correct:
-     score += 1
-     print(f"You're right! Current score: {score}")
-   else:
-     game_should_continue = False
-     print(f"Sorry, that's wrong. Final score: {score}")
-game()
+
+  user_cards = []
+  computer_cards = []
+  is_game_over = False
+
+  for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+  while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"   Your cards: {user_cards}, current score: {user_score}")
+    print(f"   Computer's first card: {computer_cards[0]}")
+
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+      is_game_over = True
+    else:
+      user_should_deal = input("What do you prefer?hit,or stand: ")
+      if user_should_deal == "hit":
+        user_cards.append(deal_card())
+      else:
+        is_game_over = True
+  while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
+
+  print(f"   Your final hand: {user_cards}, final score: {user_score}")
+  print(f"   Computer's final hand: {computer_cards}, final score: {computer_score}")
+  print(compare(user_score, computer_score))
+while input("Do you want to play a game of Blackjack? Type 'yes' or 'no': ") == "yes":
+  play_game()
